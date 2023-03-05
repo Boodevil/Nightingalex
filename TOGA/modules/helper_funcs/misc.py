6be999cc3230d2b00dbcfd71e5c4f5977f8b55ca
Boodevil <125692951+Boodevil@users.noreply.github.com>
2023-03-05 15:45:46 +0530

    Update misc.py

--- TOGA/modules/helper_funcs/misc.py
+++ TOGA/modules/helper_funcs/misc.py
@@ -76,20 +76,20 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
 
     # can only have a certain amount of buttons side by side
     if len(pairs) > 3:
-        pairs = pairs[modulo_page * 6 : 6 * (modulo_page + 1)] + [
+        pairs = pairs[modulo_page * 7 : 7 * (modulo_page + 1)] + [
             (
                 EqInlineKeyboardButton(
-                    "◁", callback_data="{}_prev({})".format(prefix, modulo_page)
+                    "≼", callback_data="{}_prev({})".format(prefix, modulo_page)
                 ),
-                EqInlineKeyboardButton("Back", callback_data="sumi_back"),
+                EqInlineKeyboardButton("Home", callback_data="toga_back"),
                 EqInlineKeyboardButton(
-                    "▷", callback_data="{}_next({})".format(prefix, modulo_page)
+                    "≽", callback_data="{}_next({})".format(prefix, modulo_page)
                 ),
             )
         ]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="sumi_back")]]
+        pairs += [[EqInlineKeyboardButton("Home", callback_data="toga_back")]]
 
     return pairs
 
