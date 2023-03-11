9010d6dcd8bcc98c832e2fc2ba8a97cd66d7e127
Boodevil <125692951+Boodevil@users.noreply.github.com>
2023-03-11 18:17:16 +0530

    Update and rename TOGA/utils/db.py to Aurora/utils/db.py

--- Aurora/utils/db.py
+++ Aurora/utils/db.py
@@ -5,19 +5,19 @@ __all__ = ['get_collection']
 import asyncio
 from motor.motor_asyncio import AsyncIOMotorClient
 from motor.core import AgnosticClient, AgnosticDatabase, AgnosticCollection
-from TOGA import MONGO_DB_URI as DB_URL
+from Aurora import MONGO_DB_URI as DB_URL
 
 print("Connecting to Database ...")
 
 _MGCLIENT: AgnosticClient = AsyncIOMotorClient(DB_URL)
 _RUN = asyncio.get_event_loop().run_until_complete
 
-if "TOGA" in _RUN(_MGCLIENT.list_database_names()):
-    print("TOGA Database Found :) => Now Logging to it...")
+if "Aurora" in _RUN(_MGCLIENT.list_database_names()):
+    print("Aurora Database Found :) => Now Logging to it...")
 else:
-    print("ROGA Database Not Found :( => Creating New Database...")
+    print("Aurora Database Not Found :( => Creating New Database...")
 
-_DATABASE: AgnosticDatabase = _MGCLIENT["TOGA"]
+_DATABASE: AgnosticDatabase = _MGCLIENT["Aurora"]
 
 
 def get_collection(name: str) -> AgnosticCollection:
