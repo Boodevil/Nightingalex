6f829fb278261b86fd4f1ff37782c9762ffe83f6
Boodevil <125692951+Boodevil@users.noreply.github.com>
2023-03-12 17:12:42 +0530

    Update and rename TOGA/modules/sql/antichannel_sql.py to Aurora/modules/sql/antichannel_sql.py

--- Aurora/modules/sql/antichannel_sql.py
+++ Aurora/modules/sql/antichannel_sql.py
@@ -4,7 +4,7 @@ from sqlalchemy import Boolean
 from sqlalchemy.sql.sqltypes import String
 from sqlalchemy import Column
 
-from TOGA.modules.sql import BASE, SESSION
+from Aurora.modules.sql import BASE, SESSION
 
 
 class AntiChannelSettings(BASE):
