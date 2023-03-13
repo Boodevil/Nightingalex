acbc11e503d64ac5e16d36311a7c846521eb8615
Boodevil <125692951+Boodevil@users.noreply.github.com>
2023-03-13 16:18:07 +0530

    Update and rename TOGA/modules/global_mutes.py to Aurora/modules/global_mutes.py

--- Aurora/modules/global_mutes.py
+++ Aurora/modules/global_mutes.py
@@ -8,11 +8,11 @@ from telegram.error import BadRequest, TelegramError
 from telegram.ext import run_async, CommandHandler, MessageHandler, Filters
 from telegram.utils.helpers import mention_html
 
-import TOGA.modules.sql.global_mutes_sql as sql
-from TOGA import dispatcher, OWNER_ID, DEV_USERS,DRAGONS,DEMONS, TIGERS,STRICT_GMUTE
-from TOGA.modules.helper_funcs.chat_status import user_admin, is_user_admin
-from TOGA.modules.helper_funcs.extraction import extract_user, extract_user_and_text
-from TOGA.modules.helper_funcs.filters import CustomFilters
+import Aurora.modules.sql.global_mutes_sql as sql
+from Aurora import dispatcher, OWNER_ID, DEV_USERS,DRAGONS,DEMONS, TIGERS,STRICT_GMUTE
+from Aurora.modules.helper_funcs.chat_status import user_admin, is_user_admin
+from Aurora.modules.helper_funcs.extraction import extract_user, extract_user_and_text
+from Aurora.modules.helper_funcs.filters import CustomFilters
 from TOGA.modules.sql.users_sql import get_all_chats
 
 GMUTE_ENFORCE_GROUP = 6
