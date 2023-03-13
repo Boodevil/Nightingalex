2056179078ba549198df61e0145921ab80d5ea71
Boodevil <125692951+Boodevil@users.noreply.github.com>
2023-03-13 16:12:15 +0530

    Update and rename TOGA/modules/dev.py to Aurora/modules/dev.py

--- Aurora/modules/dev.py
+++ Aurora/modules/dev.py
@@ -3,8 +3,8 @@ import subprocess
 import sys
 from time import sleep
 
-from TOGA import dispatcher
-from TOGA.modules.helper_funcs.chat_status import dev_plus
+from Aurora import dispatcher
+from Aurora.modules.helper_funcs.chat_status import dev_plus
 from telegram import TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
