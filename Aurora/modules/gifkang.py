7af2e8ee05ffba69d1b35b65e39e58c0f4eb4cf9
Boodevil <125692951+Boodevil@users.noreply.github.com>
2023-03-14 15:27:38 +0530

    Update and rename TOGA/modules/gifkang.py to Aurora/modules/gifkang.py

--- Aurora/modules/gifkang.py
+++ Aurora/modules/gifkang.py
@@ -4,7 +4,7 @@ from typing import Dict, List, Union
 from html import escape
 import ffmpeg
 
-from TOGA import NO_LOAD
+from Aurora import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
 
